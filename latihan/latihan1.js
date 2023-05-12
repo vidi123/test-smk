8a8369aa325428b482d571268be68a41b77a62b7
vidi123 <fadil.arofah79@gmail.com>
2023-05-12 10:43:52 +0700

    kelas tambahan tugas 1

--- latihan/latihan1.js
+++ latihan/latihan1.js
@@ -1,12 +1,14 @@
 /**
-* Menjumlahkan a dan b
-* @namespace Latihan
-* @method sum
-* @param {Number} a number
-* @param {Number} b number
-* @return {Number} hasil number
-*/
+ * Menjumlahkan a dan b
+ * @namespace Latihan
+ * @method sum
+ * @param {Number} a number
+ * @param {Number} b number
+ * @return {Number} hasil number
+ */
 function sum(a, b) {
-    // TODO: jumlahkan a dan b kemudian return hasilnya
+  // TODO: jumlahkan a dan b kemudian return hasilnya
+  let hasil = a + b;
+  return hasil;
 }
 module.exports = sum;
