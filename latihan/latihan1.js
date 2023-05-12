2a24bb890dc4eb8dc53a7b86d100f7ef3089654c
ibrahim-qordhawi <ibrahim.qordhawi@idstar.co.id>
2023-05-12 07:10:06 +0700

    initial commit

---
+++ latihan/latihan1.js
@@ -0,0 +1,12 @@
+/**
+* Menjumlahkan a dan b
+* @namespace Latihan
+* @method sum
+* @param {Number} a number
+* @param {Number} b number
+* @return {Number} hasil number
+*/
+function sum(a, b) {
+    // TODO: jumlahkan a dan b kemudian return hasilnya
+}
+module.exports = sum;
---
+++ tests/latihan1.test.js
@@ -0,0 +1,21 @@
+const sum = require("../latihan/latihan1");
+
+test('adds 1 and 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 4 and 5 to equal 9', () => {
+  expect(sum(4, 5)).toBe(9);
+});
+
+test('adds 10 and -5 to equal 5', () => {
+  expect(sum(10, -5)).toBe(5);
+});
+
+test('adds -3 and -4 to equal -7', () => {
+  expect(sum(-3, -4)).toBe(-7);
+});
+
+test('adds 2.5 and 2.5 to equal 5', () => {
+    expect(sum(2.5, 2.5)).toBe(5.0);
+})
