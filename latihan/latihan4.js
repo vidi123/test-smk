e630f9cb6b14512a05a716679ee4af67ddcbf5c1
ibrahim-qordhawi <ibrahim.qordhawi@idstar.co.id>
2023-05-16 08:15:19 +0700

    fizzbuzz

---
+++ latihan/latihan4.js
@@ -0,0 +1,14 @@
+/**
+* Melakukan looping dari angka 1 hingga n sambil mencetak nilai loop ke terminal.
+* Menghasilkan 'Fizz' ke terminal apabila nilai dapat dibagi tiga, 'Buzz' apabila dapat dibagi 5, dan 'FizzBuzz' apabila dapat dibagi tiga dan lima.
+* Selain tiga kondisi itu, maka hasilkan nilai nya langsung.
+* @namespace Latihan
+* @method fizzBuzz
+* @param {Number} n panjang loop
+*/
+function fizzBuzz(n) {
+  // TODO: lakukan looping sebanyak n kali dan cetak nilai n 
+  // atau fizz, buzz, atau fizzbuzz tergantung nilai n pada setiap iterasi
+}
+
+module.exports = fizzBuzz
