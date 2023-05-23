73419ffb1ea4af55e41daf0e1b99c81d1d02f63f
vidi123 <fadil.arofah79@gmail.com>
2023-05-23 08:05:52 +0700

    selesai latihan 5

--- latihan/latihan5.js
+++ latihan/latihan5.js
@@ -1,12 +1,36 @@
 /**
-* Hasilkan nilai faktorial dari n
-* @namespace Latihan
-* @method factorial
-* @param {Number} n number
-* @return {Number} hasil faktorial
-*/
+ * Hasilkan nilai faktorial dari n
+ * @namespace Latihan
+ * @method factorial
+ * @param {Number} n number
+ * @return {Number} hasil faktorial
+ */
 function factorial(n) {
-    // TODO: hasilkan nilai faktorial sebagai return value
-    // apabila n < 0, maka hasilkan nilia undefined
+  // TODO: hasilkan nilai faktorial sebagai return value
+  // apabila n < 0, maka hasilkan nilia undefined
+  //   let angka = "";
+  //   for (let i = n; i >= 0; i--) {
+  //     // angka
+  //     console.log((angka += i));
+  //   }
+
+  if (n < 0) {
+    return null;
+  }
+  let result = 1;
+  for (let index = n; index > 1; index--) {
+    result = result * index;
+    // console.log(index);
+  }
+  return result;
+
+  //   if(n == 0){
+  //     return 1;
+  //   }
+  //   return n * factorial(n - 1)
 }
 module.exports = factorial;
+// factorial(0);
+// factorial(1);
+factorial(5);
+// factorial(10);
